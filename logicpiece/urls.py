16d5e6a5846933d4d4214b2ecd666f6511840b9d
haseeb <haseeb@empirepixel.com>
2024-01-05 09:23:26 -0800

    small changes

--- logicpiece/urls.py
+++ logicpiece/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import  url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.urls import re_path as url
 
 urlpatterns = [
     path("", include("frontend.urls")),
